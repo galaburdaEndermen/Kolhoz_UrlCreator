a045d5458c4943470ae37c822b18c52da288dc4c
Anton Galaburda <you@example.com>
2020-04-24 13:52:43 +0300

    Enviroment seted

---
+++ Kolhoz_UrlCreator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolhoz_UrlCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Height = 0;
+            this.Width = 0;
+            this.WindowState = FormWindowState.Minimized;
+            //this.Hide();
+            this.ShowInTaskbar = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1 && e.Alt)
+            {
+              
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kolhoz_UrlCreator/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolhoz_UrlCreator
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+
+        Thread worker;
+        protected override void OnStart(string[] args)
+        {
+            worker = new Thread(Process);
+            worker.Start();
+        }
+
+        private void Process()
+        {
+            Application.Run(new Form1());
+        }
+
+        protected override void OnStop()
+        {
+            worker.Abort();
+            worker.Join(100);
+        }
+    }
+}
