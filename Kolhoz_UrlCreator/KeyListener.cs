161ded7ccec3b6d0eeb951eddf200b760819184c
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-04-24 18:24:13 +0300

    win service installing and running, but hiden winforms window doesnt propertly work switching to simple KeyListener class

---
+++ Kolhoz_UrlCreator/KeyListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolhoz_UrlCreator
+{
+    class KeyListener
+    {
+        public KeyListener()
+        {
+            List<Keys> C1 = new List<Keys> { Keys.LControlKey, Keys.N };
+            Combinaison Combinaison = new Combinaison(C1, Action);
+        }
+
+        public void Action(List<Keys> keys)
+        {
+            string Keys = "";
+            foreach (var key in keys)
+            {
+                Keys += key.ToString() + " ";
+            }
+            MessageBox.Show(Keys);
+        }
+    }
+}
--- Kolhoz_UrlCreator/Service1.cs
+++ Kolhoz_UrlCreator/Service1.cs
@@ -29,7 +29,8 @@ namespace Kolhoz_UrlCreator
 
         private void Process()
         {
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            KeyListener kl = new KeyListener();
         }
 
         protected override void OnStop()
