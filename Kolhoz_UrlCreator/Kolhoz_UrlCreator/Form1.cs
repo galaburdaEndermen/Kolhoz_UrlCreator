e34df213e0de5e86b9adaf47ac012f76d3f2103b
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-04-25 13:04:11 +0300

    second prototype

--- Kolhoz_UrlCreator/Kolhoz_UrlCreator/Form1.cs
+++ Kolhoz_UrlCreator/Kolhoz_UrlCreator/Form1.cs
@@ -15,6 +15,29 @@ namespace Kolhoz_UrlCreator
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Height = 0;
+            this.Width = 0;
+            this.WindowState = FormWindowState.Minimized;
+            //this.Hide();
+            this.ShowInTaskbar = false;
+        }
+
+
+        public void Action(List<Keys> keys)
+        {
+            string Keys = "";
+            foreach (var key in keys)
+            {
+                Keys += key.ToString() + " ";
+            }
+            MessageBox.Show(Keys);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Keys> C1 = new List<Keys> { Keys.LControlKey, Keys.N };
+            Combinaison Combinaison = new Combinaison(C1, Action);
         }
     }
 }
