28446e721e57ccb07752606887af244d66ac5fc9
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-04-25 13:00:59 +0300

    enviroment set again

---
+++ Kolhoz_UrlCreator/Kolhoz_UrlCreator/Combinaison.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Kolhoz_UrlCreator
+{
+    class Combinaison
+    {
+        public delegate void EventFunction(List<Keys> keys);
+
+        private globalKeyboardHook _gkh = new globalKeyboardHook();
+        private EventFunction _f;
+        private List<Keys> _combinaison;
+        private List<bool> _pressed = new List<bool>();
+        private bool _event = false; // акуратно убрать з кода
+
+        public Combinaison(List<Keys> combinaison, EventFunction f)
+        {
+            _f = f;
+            _combinaison = combinaison;
+            foreach (Keys k in _combinaison)
+            {
+                _gkh.HookedKeys.Add(k);
+                _pressed.Add(false);
+            }
+            _gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            _gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+        }
+
+        private void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            _event = false;
+            _pressed[_combinaison.IndexOf(e.KeyCode)] = false;
+        }
+
+        private void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            if (_combinaison.IndexOf(e.KeyCode) == _combinaison.Count - 1)
+            {
+                bool before_pressed = true;
+                for (int i = 0; i < _combinaison.IndexOf(e.KeyCode); i++)
+                {
+                    if (_pressed[i] == false)
+                    {
+                        before_pressed = false;
+                    }
+                }
+
+                //if (before_pressed == true && _event == false) може це заплатка бага
+                if (before_pressed == true)
+                {
+                    _f(_combinaison);
+                    _event = true;
+                }
+            }
+            else
+            {
+                _pressed[_combinaison.IndexOf(e.KeyCode)] = true;
+            }
+        }
+    }
+}
---
+++ Kolhoz_UrlCreator/Kolhoz_UrlCreator/HtmlRequester.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kolhoz_UrlCreator
+{
+    static class HtmlRequester
+    {
+
+        private static string getResponse(string uri)
+        {
+            WebClient wc = new WebClient();
+            return wc.DownloadString(uri);
+        }
+        public static string getTitle(string adress) // акуратніше написать
+        {
+            string res;
+            res = getResponse(adress);
+            string title = Regex.Match(res, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+            byte[] bytes = Encoding.Default.GetBytes(title);
+            title = Encoding.UTF8.GetString(bytes);
+
+            return title;
+        }
+        private static void Filtre(string fileName)
+        {
+            //удалить недопустимы символи 
+        }
+
+
+
+        public static void DisableAdapter(string interfaceName)
+        {
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = true;
+            psi.Verb = "runas";
+            p.StartInfo = psi;
+            p.Start();
+        }
+        public static bool isExist(string href)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(href) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static Thread jammer;
+
+        public static void startJamming()
+        {
+            if (jammer == null)
+            {
+                jammer = new Thread(new ThreadStart(jamming));
+                jammer.Name = "Jammer";
+                jammer.Start();
+            }
+            else
+            {
+                if (jammer.ThreadState == ThreadState.Stopped)
+                {
+                    jammer.Resume();
+                }
+            }
+        }
+        public static void stopJamming()
+        {
+            if (jammer != null)
+            {
+                jammer.Suspend();
+            }
+        }
+
+        private static void jamming()
+        {
+            DisableAdapter("Hamachi");
+            Thread.Sleep(10000);
+        }
+    }
+}
