256cdcda2fbacdc4da64ad969f0b445b24a5893b
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-04-26 15:45:02 +0300

    UrlRunner added

---
+++ Kolhoz_UrlCreator/UrlRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UrlRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(args[0]);
+            //System.Diagnostics.Process.Start("http://192.168.1.1");
+            //Thread.Sleep(1);
+        }
+    }
+}
